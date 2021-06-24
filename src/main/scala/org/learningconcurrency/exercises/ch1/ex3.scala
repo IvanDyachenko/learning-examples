fb2dcdc914b2bbb136324d11253046b5f17a4a88
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-06-24 14:53:57 +0300

    Chapter 1. Exercise 3: Implement a `check` method

--- src/main/scala/org/learningconcurrency/exercises/ch1/ex3.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch1/ex3.scala
@@ -2,8 +2,22 @@ package org.learningconcurrency
 package exercises
 package ch1
 
-object Ex3 extends App {
+object Ex3a extends App {
 
-  def check2[T](xs: Seq[T])(pred: T => Boolean): Boolean = ???
+  def check[T](xs: Seq[T])(pred: T => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(xs: Seq[T]): Boolean = xs match {
+      case x :: xs => pred(x) && go(xs)
+      case _       => true
+    }
+
+    go(xs)
+  }
+
+}
+
+object Ex3b extends App {
+
+  def check[T](xs: Seq[T])(pred: T => Boolean): Boolean = xs.exists(pred)
 
 }
