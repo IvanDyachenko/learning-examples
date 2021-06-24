e16e8fddd755f6841721a28f53e072176e972756
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-06-24 14:48:08 +0300

    Chapter 1. Exercise 2: Implement a `fuse` method

--- src/main/scala/org/learningconcurrency/exercises/ch1/ex2.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch1/ex2.scala
@@ -2,8 +2,18 @@ package org.learningconcurrency
 package exercises
 package ch1
 
-object Ex2 extends App {
+object Ex2a extends App {
 
-  def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = ???
+  def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = a.flatMap(a => b.map(a -> _))
+
+}
+
+object Ex2b extends App {
+
+  def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] =
+    for {
+      a <- a
+      b <- b
+    } yield (a, b)
 
 }
