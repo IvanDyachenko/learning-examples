10b58d9960be518f8a0ebf32c548195710029786
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-06-24 14:10:25 +0300

    Chapter 1. Exercise 1: Implement a `compose` method

--- src/main/scala/org/learningconcurrency/exercises/ch1/ex1.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch1/ex1.scala
@@ -4,12 +4,12 @@ package ch1
 
 object Ex1a extends App {
 
-  def compose[A, B, C](g: B => C, f: A => B): A => C = ???
+  def compose[A, B, C](g: B => C, f: A => B): A => C = (a: A) => g(f(a))
 
 }
 
 object Ex1b extends App {
 
-  def compose[A, B, C](g: B => C, f: A => B): A => C = ???
+  def compose[A, B, C](g: B => C, f: A => B): A => C = g compose f
 
 }
